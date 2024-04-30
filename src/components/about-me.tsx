9ba94a506ca8e7153a3561418a031fd371961fe8
Stephanie Borchers <stephanie@Stephanies-Air.MG8702>
2024-04-30 09:07:55 -0400

    The starting skeleton of my website

---
+++ src/components/about-me.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+function AboutMe() {
+  return (
+    <main>
+      <h2>About Me</h2>
+      <p>
+        I'm Stephanie Borchers, a software engineer with experience in
+        web-application development. Before software, I practiced structural
+        engineering, and have worked on the structural design for over 15
+        buildings across the US. My passion for software and technology
+        developed from the desire to automate processes and improve efficiency.
+      </p>
+    </main>
+  );
+}
+
+export default AboutMe;
---
+++ src/components/projects.tsx
@@ -0,0 +1,44 @@
+import React from "react";
+
+function Projects() {
+  return (
+    <nav>
+      <h2>Projects</h2>
+      <p>
+        <strong>Software</strong>
+      </p>
+      <a
+        href="https://www.linkedin.com/in/stephanie-borchers-pe-88987869/"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Gallery App
+      </a>
+      |
+      <a
+        href="https://github.com/sborchers/TDI-Capstone"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Success Prediction for Airbnb Listings
+      </a>
+      <p>
+        <strong>Built Environment</strong>
+      </p>
+      <p>MFAH</p>
+      <p>IAS</p>
+      <p>Day's End</p>
+      <img
+        src="/Users/stephanie/Documents/Projects/React/personal-website/sb-website/src/images/days-end.jpg"
+        alt="Day's End Photo"
+      />
+      <p>SLT</p>
+      <p>MFAH</p>
+      <p>LMC</p>
+      <p>MPK22</p>
+      <p>G5 Garage</p>
+    </nav>
+  );
+}
+
+export default Projects;
