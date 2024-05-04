f4f8be0c97eab0407c9c1fb0406850f4843918fc
Stephanie <stephanie@outerlabs.io>
2024-05-04 18:40:49 -0400

    Menu Items - added buttons to toolbar

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { AppBar, IconButton } from "@mui/material";
+import { AppBar, Button, IconButton, Menu, MenuItem } from "@mui/material";
 import { ReactComponent as Moon } from "../../icons/moon.svg";
 import { ReactComponent as Sun } from "../../icons/sun.svg";
 
@@ -8,29 +8,47 @@ interface HeaderProps {
   toggleDarkMode: () => void;
 }
 
+const projectOptions = ["Software", "Built Environment"];
+
 const Header: React.FC<HeaderProps> = ({ darkMode, toggleDarkMode }) => {
+  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+  const open = Boolean(anchorEl);
   const handleClick = () => {
     toggleDarkMode();
   };
+  const handleOpen = (event: React.MouseEvent<HTMLLIElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
 
   return (
-    <AppBar
-      position="static"
-      style={{
-        backgroundColor: darkMode ? "purple" : "orangered",
-        flexDirection: "row",
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <h1>Hey There!</h1>
-      <IconButton
-        onClick={handleClick}
-        style={{ position: "absolute", right: 10 }}
+    <>
+      <AppBar
+        position="static"
+        style={{
+          backgroundColor: darkMode ? "purple" : "orangered",
+          flexDirection: "row",
+          padding: "0px 10px",
+          justifyContent: "space-between",
+          alignItems: "center",
+        }}
       >
-        {darkMode ? <Sun /> : <Moon />}
-      </IconButton>
-    </AppBar>
+        <h1>Hey There!</h1>
+        <MenuItem>About</MenuItem>
+        <MenuItem>Skills</MenuItem>
+        <MenuItem onClick={handleOpen}>Projects</MenuItem>
+        <IconButton onClick={handleClick}>
+          {darkMode ? <Sun /> : <Moon />}
+        </IconButton>
+      </AppBar>
+      <Menu anchorEl={anchorEl} open={Boolean(anchorEl)} onClose={handleClose}>
+        {projectOptions.map((option) => (
+          <MenuItem>{option}</MenuItem>
+        ))}
+      </Menu>
+    </>
   );
 };
 
